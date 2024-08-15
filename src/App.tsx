854a794c4c524eb944875257dff3a51a6f3edb45
Aryan Thacker <randomthacker@gmail.com>
2024-08-15 23:32:02 +0530

    feat: completed app

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
-import './App.css'
-import TweetWall from './components/tweet-wall/tweet-wall'
+import "./App.css";
+import LeftSideBar from "./components/left-sidebar/left-sidebar";
+import MessageDrawer from "./components/message/message-drawer";
+import TweetWall from "./components/main-section/tweet-wall/tweet-wall";
+import RightSideBar from "./components/right-sidebar/right-sidebar";
 
 function App() {
-
   return (
-    <div className=' h-screen w-full no-scrollbar'>
-      <TweetWall/>
-    </div>
-  )
+    <>
+      <div className="flex justify-between h-screen w-full no-scrollbar">
+        <div className="w-[35%] h-full overflow-hidden">
+          <LeftSideBar />
+        </div>
+        <div className="w-[100%] overflow-y-scroll no-scrollbar py-4">
+          <TweetWall />
+        </div>
+        <div className="w-[35%] overflow-hidden">
+          <RightSideBar />
+        </div>
+      </div>
+      <MessageDrawer />
+    </>
+  );
 }
 
-export default App
+export default App;
