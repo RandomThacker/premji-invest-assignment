8aa0495b603cd83143309526f41c02f8288ecfc6
Aryan Thacker <randomthacker@gmail.com>
2024-08-15 13:43:09 +0530

    update: added center section

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import CenterSection from './components/center-section'
+import { DatePicker } from 'antd';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      test
-    </>
+    <div className=' h-screen w-full'>
+      <CenterSection/>
+    </div>
   )
 }
 
