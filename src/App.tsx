a90bb0caddc1dae18b15a9ee90e0f730396fe89e
Aryan Thacker <randomthacker@gmail.com>
2024-08-15 14:06:38 +0530

    refactor: added css

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import './App.css'
 import CenterSection from './components/center-section'
-import { DatePicker } from 'antd';
 
 function App() {
 
   return (
-    <div className=' h-screen w-full'>
+    <div className=' h-screen w-full no-scrollbar'>
       <CenterSection/>
     </div>
   )
