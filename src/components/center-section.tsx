87081b54c0a9b90055d321ba834f31c2bbd7737a
Aryan Thacker <randomthacker@gmail.com>
2024-08-15 14:06:59 +0530

    refactor: updated path

--- src/components/center-section.tsx
+++ src/components/center-section.tsx
@@ -1,17 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import TweetCard from './tweet-card';
-import tweetData from '../utils/data.json'; // Import your JSON data
+import tweetData from '../utils/tweet-data.json';
 import { TweetType } from '../utils/tweet-card.types';
 
 const CenterSection: React.FC = () => {
   const [tweets, setTweets] = useState<TweetType[]>([]);
 
   useEffect(() => {
-    setTweets(tweetData); // Assign the JSON data to the state
+    setTweets(tweetData);
   }, []);
 
   return (
-    <div className='flex items-center flex-col'>
+    <div className='flex items-center flex-col no-scrollbar'>
       {tweets.map((tweet, index) => (
         <TweetCard key={index} tweet={tweet} />
       ))}
